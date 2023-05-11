b40351f37e4ab29567b61c58d554f00ead139c85
anananass <ananauze@gmail.com>
2023-05-11 12:24:19 +0400

    Add project files.

---
+++ MassiveTask6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassiveTask6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int arrayLenght = 10;
+            int[] numbers = new int[arrayLenght];
+            Random arrayRandom = new Random();
+            int minNumberInArray = 0;
+            int maxNumberInArray = 30;
+            int tempNumber = 0;
+
+            Console.WriteLine("\nВыводим для начала сам массив:");
+
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                numbers[i] = arrayRandom.Next(minNumberInArray, maxNumberInArray);
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine("\n\nТеперь выведем отсортированный массив:");
+            
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                for (int j = i + 1; j < numbers.Length; j++) 
+                {
+                    if (numbers[i] > numbers[j]) 
+                    {
+                        tempNumber = numbers[j];
+                        numbers[j] = numbers[i];
+                        numbers[i] = tempNumber;
+                    }
+                }
+                
+                Console.Write(numbers[i] + " ");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
